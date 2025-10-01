2388d21006801cd6722b2b6f4b8a64f1845fcdf5
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2025-10-01 18:50:16 +0300

    feat: add accordions

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -29,3 +29,46 @@ links.forEach((link) => {
     body.classList.remove('menu-open');
   });
 });
+
+// Slider
+
+// Accordions
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionItems = document.querySelectorAll('.faq__item');
+  const KEY = 'activeAccordionIndex';
+
+  function closeAllAccordions() {
+    accordionItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+  }
+
+  function openAccordion(index) {
+    const item = accordionItems[index];
+
+    item.classList.add('active');
+    localStorage.setItem(KEY, index.toString());
+  }
+
+  accordionItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      const isActive = item.classList.contains('active');
+
+      if (isActive) {
+        return;
+      }
+
+      closeAllAccordions();
+      openAccordion(index);
+    });
+  });
+
+  const savedIndex = localStorage.getItem(KEY);
+  if (savedIndex !== null) {
+    closeAllAccordions();
+    openAccordion(parseInt(savedIndex));
+  } else {
+    openAccordion(0);
+  }
+});
