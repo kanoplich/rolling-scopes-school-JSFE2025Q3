7b64cddcbff9c8d6ae1c8fc695192df1b4b777a6
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2025-11-01 17:49:32 +0300

    feat: add sounds and play sounds

--- virtual-music-kit/script.js
+++ virtual-music-kit/script.js
@@ -1,3 +1,51 @@
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const sounds = new Map();
+
+const drumKitData = {
+  A: { sound: 'kick', name: 'Kick' },
+  S: { sound: 'snare', name: 'Snare' },
+  D: { sound: 'rimshot', name: 'Rimshot' },
+  F: { sound: 'hihat', name: 'Hi-Hat' },
+  G: { sound: 'clap', name: 'Clap' },
+  H: { sound: 'crash', name: 'Crash' },
+  J: { sound: 'ride', name: 'Ride' },
+  K: { sound: 'high-tom', name: 'High Tom' },
+  L: { sound: 'low-tom', name: 'Low Tom' },
+};
+
+async function loadSounds() {
+  try {
+    for (let key in drumKitData) {
+      const soundName = drumKitData[key].sound;
+      const soundPath = `assets/sounds/${soundName}.mp3`;
+
+      const response = await fetch(soundPath);
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+      sounds.set(soundName, audioBuffer);
+    }
+  } catch (error) {
+    console.error('Onload sounds error:', error);
+  }
+}
+
+function playSound(soundName) {
+  if (!sounds.has(soundName)) return;
+
+  const source = audioContext.createBufferSource();
+  const gainNode = audioContext.createGain();
+
+  source.buffer = sounds.get(soundName);
+  gainNode.gain.value = 1.0;
+
+  source.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+
+  source.start();
+  return source;
+}
+
 function createElement(type, style, text = '') {
   const element = document.createElement(type);
   element.classList.add(style);
@@ -21,7 +69,41 @@ titleWrapper.append(title);
 const drumKit = createElement('div', 'drum-kit');
 container.append(drumKit);
 
-for (let i = 0; i < 9; i++) {
+for (let key in drumKitData) {
   const pad = createElement('div', 'pad');
+  const padKey = createElement('div', 'pad-key', `${key}`);
+  const padTitle = createElement('h3', 'pad-title', `${drumKitData[key].name}`);
+  pad.dataset.key = key;
+  pad.dataset.sound = drumKitData[key].sound;
+
   drumKit.append(pad);
+  pad.append(padKey);
+  pad.append(padTitle);
 }
+
+drumKit.addEventListener('click', (event) => {
+  const pad = event.target.closest('.pad');
+
+  if (!pad) return;
+
+  const soundName = pad.dataset.sound;
+  playSound(soundName);
+});
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key.toUpperCase();
+  if (event.repeat) return;
+
+  if (drumKitData[key]) {
+    const soundName = drumKitData[key].sound;
+    playSound(soundName);
+
+    const pad = document.querySelector(`.pad[data-key="${key}"]`);
+    if (pad) {
+      pad.classList.add('active');
+      setTimeout(() => pad.classList.remove('active'), 150);
+    }
+  }
+});
+
+await loadSounds();
