8b21ac59c525976b9394d286c26f09f1cc063ae5
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2025-11-02 16:16:47 +0300

    feat: add edit key

--- virtual-music-kit/script.js
+++ virtual-music-kit/script.js
@@ -66,6 +66,9 @@ const titleIcon = createElement('div', 'title-icon');
 titleWrapper.append(titleIcon);
 titleWrapper.append(title);
 
+const keyError = createElement('span', 'key-error', 'text error');
+container.append(keyError);
+
 const drumKit = createElement('div', 'drum-kit');
 container.append(drumKit);
 
@@ -73,26 +76,43 @@ for (let key in drumKitData) {
   const pad = createElement('div', 'pad');
   const padKey = createElement('div', 'pad-key', `${key}`);
   const padTitle = createElement('h3', 'pad-title', `${drumKitData[key].name}`);
+  const padIcon = createElement('div', 'pad-icon');
   pad.dataset.key = key;
   pad.dataset.sound = drumKitData[key].sound;
 
   drumKit.append(pad);
   pad.append(padKey);
   pad.append(padTitle);
+  pad.append(padIcon);
 }
+
+let isEdit = false;
 
 drumKit.addEventListener('click', (event) => {
   const pad = event.target.closest('.pad');
+  const edit = event.target.closest('.pad-icon');
 
-  if (!pad) return;
+  if (edit && isEdit) return;
+
+  if (edit) {
+    editKey(pad);
+  }
+
+  if (!pad || isEdit) return;
 
   const soundName = pad.dataset.sound;
   playSound(soundName);
+
+  if (pad) {
+    pad.classList.add('active');
+    setTimeout(() => pad.classList.remove('active'), 150);
+  }
 });
 
 document.addEventListener('keydown', (event) => {
+  console.log(event.code);
   const key = event.key.toUpperCase();
-  if (event.repeat) return;
+  if (event.repeat || isEdit) return;
 
   if (drumKitData[key]) {
     const soundName = drumKitData[key].sound;
@@ -105,5 +125,57 @@ document.addEventListener('keydown', (event) => {
     }
   }
 });
+
+function editKey(pad) {
+  isEdit = true;
+
+  const padKeyElement = pad.querySelector('.pad-key');
+  const currentKey = pad.dataset.key;
+
+  const input = createElement('input', 'pad-input');
+  input.type = 'text';
+  input.value = currentKey;
+  input.maxLength = 1;
+
+  padKeyElement.innerText = '';
+  padKeyElement.appendChild(input);
+
+  input.focus();
+  input.select();
+
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const regexLetters = /^[A-Z]$/;
+      const newKey = input.value.toUpperCase();
+
+      if (newKey === currentKey) {
+        keyError.classList.remove('show');
+        finishEditKey(newKey, padKeyElement);
+      } else if (!regexLetters.test(newKey)) {
+        keyError.innerText = 'You need to use English letters';
+        keyError.classList.add('show');
+      } else if (newKey !== currentKey && drumKitData[newKey]) {
+        keyError.innerText = `The letter ${newKey} is already in use`;
+        keyError.classList.add('show');
+      } else {
+        keyError.classList.remove('show');
+        updateKey(pad, newKey, currentKey);
+        finishEditKey(newKey, padKeyElement);
+      }
+    }
+  });
+}
+
+function finishEditKey(key, element) {
+  element.innerHTML = '';
+  element.innerText = key;
+  isEdit = false;
+}
+
+function updateKey(element, key, oldKey) {
+  element.dataset.key = key;
+  drumKitData[key] = drumKitData[oldKey];
+  delete drumKitData[oldKey];
+}
 
 await loadSounds();
