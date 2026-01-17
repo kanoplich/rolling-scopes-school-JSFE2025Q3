ef3ccf669563fc816c5ade3215a13df48edd3515
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-01-17 19:29:02 +0300

    init: start async race task

---
+++ async-race/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      'unicorn/better-regex': 'warn',
+    },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
